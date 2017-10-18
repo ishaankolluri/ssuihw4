130414adb3100a506f02a49e46ef93afb6bb978f
Ishaan Kolluri <ishaankolluri@gmail.com>
2017-10-18 01:18:58 -0400

    Add json file

--- hw4/src/globalflavors.js
+++ hw4/src/globalflavors.js
@@ -1,39 +1,9 @@
+
 class GlobalFlavors {
   constructor(){
-    this.flavors = [
-      "caramelpecan@2x.jpg",
-      "mapleapple@2x.jpg",
-      "original@2x.jpg",
-      "vegan@2x.jpg",
-      "gluten@2x.jpg",
-      "bacon@2x.jpg",
-      "cranberry@2x.jpg",
-      "buttermilk@2x.jpg",
-      "bday@2x.jpg",
-      "carrot@2x.jpg",
-      "lemon@2x.jpg",
-      "pumpkin@2x.jpg",
-      "strawberry@2x.jpg",
-      "walnut@2x.jpg",
-      "blackberry@2x.jpg",
-    ];
-    this.names = [
-      "Caramel Pecan",
-      "Maple Apple Pecan",
-      "Original",
-      "Original(Vegan)",
-      "Original(Gluten-Free)",
-      "Bacon",
-      "Cranberry",
-      "Buttermilk",
-      "Birthday Cake",
-      "Carrot Cake",
-      "Lemon",
-      "Pumpkin Spice",
-      "Strawberry",
-      "Walnut",
-      "Blackberry",
-    ]
+    var data = require('./data.json');
+    this.flavors = data.flavors;
+    this.names = data.names;
   }
 }
 
